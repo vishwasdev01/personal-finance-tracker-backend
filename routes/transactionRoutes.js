59d724760f2275adc408dfcc60ff0d7991336d11
vishwasdev01 <vishwasdev01@gmail.com>
2025-03-07 12:16:10 +0530

    changes

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const transactionController = require('../controller/transactionController');
-
 const router = express.Router();
 
 router.post('/', transactionController.createTransaction);
